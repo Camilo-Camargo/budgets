6be1d68900a4bba6f920b6a865ccdef4e6d3e52b
camilo-camargo <camilocamargo49@gmail.com>
2023-09-19 07:55:49 -0500

    [core] add mysql layer and console user [core] add gitignore

---
+++ packages/core/budgets-web/src/main/java/org/learnofficial/BudgetsBackendApplication.java
@@ -0,0 +1,7 @@
+package org.learnofficial;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("")
+public class BudgetsBackendApplication extends Application{}
